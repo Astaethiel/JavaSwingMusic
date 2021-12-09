5da30d29414abbaaadb72a3a1e7993a60f3e215a
Astaethiel <marciniak.miko@gmail.com>
2021-12-09 12:28:53 +0000

    Created artist fields from task 1.2

---
+++ mikolaj.marciniak/src/main/java/CS2020/assignment2/Artist.java
@@ -0,0 +1,17 @@
+package CS2020.assignment2;
+
+import java.util.*;
+
+public class Artist {
+    UUID artistID;
+    String firstName;
+    String lastName;
+    String dob;
+    String placeOfBirth;
+    ArrayList<String> songs = new ArrayList<>();
+    
+    Artist(){
+
+    }
+    
+}
