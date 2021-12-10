da62b7cd319e5a01fc0c0ab5c9fe49c81e1f1b20
Astaethiel <marciniak.miko@gmail.com>
2021-12-10 13:22:46 +0000

    Completed 2.3 (part2)

--- mikolaj.marciniak/src/main/java/CS2020/assignment2/Utils.java
+++ mikolaj.marciniak/src/main/java/CS2020/assignment2/Utils.java
@@ -1,10 +1,12 @@
 package CS2020.assignment2;
 
 import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Utils {
     public static HashMap returnSongDurationAndTitleFormatted(ArrayList<Song> songList) {
-        HashMap<UUID, String> map=new HashMap<UUID,String>();//Creating HashMap
+        HashMap<UUID, String> map=new HashMap<UUID,String>();
         for (Song song : songList) {
             Integer minutes = song.getDuration()/60;
             Integer seconds = song.getDuration()%60;
@@ -12,9 +14,16 @@ public class Utils {
             String titleString = song.getTitle() + " " + "(" + time + ")";
             map.put(song.getSongID(), titleString);
         }    
-        }
-        
-        
+        return map;
     }
-    
+
+    public Boolean checkIfBornOnWeekend(String dob){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH);
+        LocalDate dateOfBirth = LocalDate.parse(dob, formatter);
+        String day = dateOfBirth.getDayOfWeek().toString();
+        if (day == "SATURDAY" || day == "SUNDAY") {
+            return true;
+        }
+        return false;
+    } 
 }
