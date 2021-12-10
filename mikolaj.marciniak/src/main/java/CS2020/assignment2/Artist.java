31b4b423c86e85b51038b3e339f12e88bf0d0aa2
Astaethiel <marciniak.miko@gmail.com>
2021-12-10 11:39:03 +0000

    Comp[leted task 1.4

--- mikolaj.marciniak/src/main/java/CS2020/assignment2/Artist.java
+++ mikolaj.marciniak/src/main/java/CS2020/assignment2/Artist.java
@@ -3,15 +3,55 @@ package CS2020.assignment2;
 import java.util.*;
 
 public class Artist {
-    UUID artistID;
-    String firstName;
-    String lastName;
-    String dob;
-    String placeOfBirth;
-    ArrayList<String> songs = new ArrayList<>();
+    private UUID artistID;
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private String placeOfBirth;
+    private ArrayList<Song> songs = new ArrayList<Song>();
 
     Artist(){
        artistID = UUID.randomUUID();
+       }
+
+    public UUID getArtistID(){
+        return artistID; 
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getDOB(){
+        return dob;
+    }
+    public String getPlaceOfBirth(){
+        return placeOfBirth;
+    }
+    public ArrayList<Song> getSongs(){
+        return songs;
+    }
+    
+
+
+    public void setArtistID(UUID ID){
+        artistID = ID; 
+    }
+    public void setFirstName(String fname){
+        firstName = fname;
+    }
+    public void setLastName(String lname){
+        lastName = lname;
+    }
+    public void setDOB(String DOB){
+        dob = DOB;
+    }
+    public void setPlaceOfBirth(String place){
+        placeOfBirth = place;
+    }
+    public void setSongs(ArrayList<Song> songList){
+        songs = songList;
     }
     
 }
--- mikolaj.marciniak/src/main/java/CS2020/assignment2/Song.java
+++ mikolaj.marciniak/src/main/java/CS2020/assignment2/Song.java
@@ -3,8 +3,37 @@ package CS2020.assignment2;
 import java.util.*;
 
 public class Song {
-    UUID songID;
-    UUID artistID;
-    String title;
-    Integer duration;
+    private UUID songID;
+    private UUID artistID;
+    private String title;
+    private Integer duration;
+
+    public UUID getSongID(){
+        return songID; 
+    }
+    public UUID getArtistID(){
+        return artistID; 
+    }
+    public String getTitle(){
+        return title;
+    }
+    public Integer getDuration(){
+        return duration;
+    }
+   
+   
+   
+   
+    public void setSongID(UUID ID){
+        songID = ID; 
+    }
+    public void setArtistID(UUID ID){
+        artistID = ID; 
+    }
+    public void setTitle(String newTitle){
+        title = newTitle;
+    }
+    public void getDuration(Integer newDuration){
+        duration = newDuration;
+    }
 }
