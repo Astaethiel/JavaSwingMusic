899a05d60b9fa87fdb0d1eb48dc293aeba56ff54
Astaethiel <marciniak.miko@gmail.com>
2021-12-10 13:22:25 +0000

    Completed 2.3

--- mikolaj.marciniak/src/main/java/CS2020/assignment2/App.java
+++ mikolaj.marciniak/src/main/java/CS2020/assignment2/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Utils u = new Utils();
+        //System.out.print(u.checkIfBornOnWeekend("3 Dec 1999"));
     }
 }
